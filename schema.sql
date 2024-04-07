97c27e66e8e7d771c17821d93c91cba23531a95f
hcaatu <aatu.hokkane@gmail.com>
2024-04-07 23:19:45 +0300

    update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,3 @@
 CREATE TABLE restos (id SERIAL PRIMARY KEY, name TEXT, location TEXT, created_at TIMESTAMP);
 CREATE TABLE reviews (id SERIAL PRIMARY KEY, resto_id INT, content TEXT, sent_at TIMESTAMP, sent_by TEXT, visible BOOLEAN);
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username UNIQUE TEXT, password TEXT);
